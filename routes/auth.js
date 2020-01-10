576cbc387bb87d94921d51e41d2c9a90f815089c
Никонов Владислав <39374425+nikonov1996@users.noreply.github.com>
2020-01-10 13:52:31 +0700

    шифрование паролей

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 const router = Router();
 
@@ -21,7 +22,7 @@ router.post('/login', async (req,res)=>{
     const candidate = await User.findOne({email})
 
     if (candidate) {
-      const areSame = password === candidate.password
+      const areSame = await bcrypt.compare(password,candidate.password)
       if (areSame) {
         req.session.user = candidate;
         req.session.isAuthenticated = true;
@@ -50,8 +51,11 @@ router.post('/regist', async (req, res)=>{
     if (candidate) {
       res.redirect('/auth/login#register')
     } else {
+      const hashPassword = await bcrypt.hash(password, 10)
       const user = new User({
-        email,name,password,
+        email,
+        name,
+        password: hashPassword,
         cart: {items:[]}
       })
       await user.save()
